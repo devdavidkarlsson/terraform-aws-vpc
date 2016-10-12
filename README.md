2846bdce7f359e596a69bac47c654326dd014077
David Karlsson <david.karlsson@netlight.com>
2016-10-12 09:57:44 +0200

    Updated variables for 3 AZ with prublic, private, spare subnets

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # terraform-aws-vpc
 
 This repository contains a [Terraform][] project that builds [Scenario 2: VPC
-with Public and Private Subnets][scenario_two] from the [AWS documentation][].
+with Public and Private Subnets over 3 accessibility zones][scenario_two] from the [AWS documentation][].
 It's from [this blog post][blog_post] describing how it all works and is
 designed to give a working example which can the basis of something much more
 complex.
@@ -27,10 +27,6 @@ terraform apply -var-file terraform.tfvars
 ```
 terraform destroy -var-file terraform.tfvars
 ```
-
-## Author
-
-Copyright (c) 2015 Nick Charlton <nick@nickcharlton.net>. MIT Licensed.
 
 [Terraform]: http://terraform.io
 [scenario_two]: http://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/VPC_Scenario2.html
--- variables.tf
+++ variables.tf
@@ -17,15 +17,64 @@ variable "amis" {
 
 variable "vpc_cidr" {
     description = "CIDR for the whole VPC"
-    default = "10.0.0.0/16"
+    default = "10.0.0.0/16" 
 }
 
-variable "public_subnet_cidr" {
-    description = "CIDR for the Public Subnet"
-    default = "10.0.0.0/24"
+
+/*
+  Public subnets:
+*/
+
+variable "public_subnet_cidr_a" {
+    description = "CIDR for the Public Subnet in AZ A"
+    default = "10.0.32.0/20"
 }
 
-variable "private_subnet_cidr" {
-    description = "CIDR for the Private Subnet"
-    default = "10.0.1.0/24"
+variable "public_subnet_cidr_b" {
+    description = "CIDR for the Public Subnet in AZ B"
+    default = "10.0.96.0/20"
 }
+
+variable "public_subnet_cidr_c" {
+    description = "CIDR for the Public Subnet in AZ C"
+    default = "10.0.160.0/20"
+}
+
+/*
+  Private subnets:
+*/
+
+variable "private_subnet_cidr_a" {
+    description = "CIDR for the Private Subnet in AZ A"
+    default = "10.0.0.0/19"
+}
+
+variable "private_subnet_cidr_b" {
+    description = "CIDR for the Private Subnet in AZ B"
+    default = "10.0.64.0/19"
+}
+
+variable "private_subnet_cidr_c" {
+    description = "CIDR for the Private Subnet in AZ C"
+    default = "10.0.128.0/19"
+}
+
+/*
+  Private subnets:
+*/
+/*
+variable "spare_subnet_cidr_a" {
+    description = "CIDR for the Private Subnet in AZ A"
+    default = "10.0.98.0/20"
+}
+
+variable "spare_subnet_cidr_b" {
+    description = "CIDR for the Private Subnet in AZ B"
+    default = "10.0.112.0/20"
+}
+
+variable "spare_subnet_cidr_c" {
+    description = "CIDR for the Private Subnet in AZ C"
+    default = "10.0.176.0/20"
+}
+*/
